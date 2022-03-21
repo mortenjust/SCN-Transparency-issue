262eb8a011a0071ba925241931270c1c2e18bfec
Morten Just <morten.just@genstart.dk>
2022-03-21 09:53:26 +0100

    Initial Commit

---
+++ SCN Transparent/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SCN Transparent
+//
+//  Created by Morten Just on 3/21/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SCN Transparent/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SCN Transparent
+//
+//  Created by Morten Just on 3/21/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
